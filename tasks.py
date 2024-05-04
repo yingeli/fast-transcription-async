1003f0b56b7ed400369fe8c5ef65b5be1fcc7ff2
yingeli <yingeli@hotmail.com>
2024-05-04 06:33:53 +0000

    dev

--- tasks.py
+++ tasks.py
@@ -3,10 +3,6 @@ import os
 from celery import Celery
 from transcript import transcript
 
-#celery = Celery("transcript")
-#celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379")
-#celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379")
-
 broker = os.environ.get("CELERY_BROKER_URL", "redis://127.0.0.1:6379")
 backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://127.0.0.1:6379")
 transcriptions = Celery(__name__, broker=broker, backend=backend)
