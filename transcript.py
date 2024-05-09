83a5d62bcd9da81dcef9ad5eecd357cbc51d1805
yingeli <yingeli@hotmail.com>
2024-05-09 20:11:48 +0800

    dev

--- transcript.py
+++ transcript.py
@@ -30,7 +30,7 @@ class HTTPError(Exception):
 def transcript(audio_uri, config, speech_service_key):
     try:
         access_token = get_access_token()
-        # headers = {"Authorization": f"Bearer {access_token}"}
+        headers = {"Authorization": f"Bearer {access_token}"}
         with requests.get(audio_uri, headers=headers, stream=True) as resp:  
             # ensure the request was successful  
             resp.raise_for_status()
@@ -51,8 +51,8 @@ async def transcript_async(audio_uri, config, speech_service_key):
     try:
         async with aiohttp.ClientSession() as session:
             access_token = get_access_token()
-            #headers = {"Authorization": f"Bearer {access_token}"}
-            async with session.get(audio_uri) as get_response:  
+            headers = {"Authorization": f"Bearer {access_token}"}
+            async with session.get(audio_uri, headers=headers) as get_response:  
                 get_response.raise_for_status()
                 stream = get_response.content
 
