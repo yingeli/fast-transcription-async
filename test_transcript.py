221ad6f8362f5518e88d59e8b490fbfc94e7e8b8
yingeli <yingeli@hotmail.com>
2024-05-10 17:52:21 +0800

    dev

--- test_transcript.py
+++ test_transcript.py
@@ -1,8 +1,8 @@
 import time
 #import asyncio
-from transcript import transcript_httpx
+from transcript import transcript
 
-key = ""
+key = "275321276dcd4e438c734cdddb5e5985"
 audio_uri = "https://oppobatchasrblobstorage.blob.core.windows.net/audios/en/english_30mins.wav?sp=r&st=2024-04-24T04:09:39Z&se=2024-12-31T12:09:39Z&spr=https&sv=2022-11-02&sr=b&sig=2RW0PzhJ3BGAqSuQvLQ%2Fz5NWrKhXOWwoOTd2TdkQiug%3D"
 locale = "en-US"
 definition = {
@@ -13,7 +13,7 @@ definition = {
 }
 start_time = time.time()
 
-response = transcript_httpx(audio_uri, definition, key)
+response = transcript(audio_uri, definition, key)
 #loop = asyncio.get_event_loop()  
 #response = loop.run_until_complete(transcript_async(audio_uri, definition, key))  
 
